f53903a2cc9fc11e15b5893336623cd2885ef849
Sojin Lee <isjift55@ewhain.net>
2022-07-05 01:40:58 +0900

    ✍️ router

---
+++ src/routes/routes.js
@@ -0,0 +1,16 @@
+const routes = {
+  MAIN: "/",
+
+  LOGIN: "login/",
+  JOIN: "join/",
+  FINDID: "findid/",
+  FINDPW: "findpw/",
+
+  HOME: "home/",
+
+  MAKENEW: "makenew/",
+
+  MYPAGE: "mypage/",
+};
+
+export default routes;
---
+++ src/ui/pages/MainPage.js
@@ -0,0 +1,5 @@
+export default function MainPage() {
+  return <div>Main Page</div>;
+}
+
+// 여기서 로그인 버튼을 누르면 login 페이지로 넘어갑니다.
