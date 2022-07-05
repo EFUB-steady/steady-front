7aea3c6f2119fc4ffc93dfc0be55b527d35d3b54
Haein Kwon <hae0_02ni@ewhain.net>
2022-07-05 16:42:27 +0900

    texts.js

--- src/core/texts.js
+++ src/core/texts.js
@@ -1,8 +1,134 @@
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
+
+/*Title*/
 
 export const Title1 = styled.div`
+  font-family: 'Pretendard';
+  font-style: normal;
   font-weight: 700;
   font-size: 40px;
-  line-height: 40px;
-  color: #000;
+  line-height: 100%;
 `;
+
+export const Title2 = styled.div`
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 40px;
+line-height: 100%;
+`;
+
+export const Title3 = styled.div `
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 18px;
+line-height: 100%;
+`;
+
+export const Title5 = styled.div`
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 10px;
+line-height: 160%;
+`;
+
+/*Body*/
+
+export const Body = styled.div`
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 400;
+font-size: 10px;
+line-height: 160%;
+`;
+
+export const Body1 = styled.div`
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 200%;
+`;
+
+export const Body2 = styled.div`
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 200%;
+`;
+
+export const Body3 = styled.div`
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 100%;
+`;
+
+/*Subtitle*/
+
+export const Subtitle1 = styled.div`
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 21px;
+line-height: 100%;
+`;
+
+export const Subtitle2 = styled.div`
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 18px;
+line-height: 100%;
+`;
+
+export const Subtitle3 = styled.div`
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 14px;
+line-height: 100%;
+`;
+
+export const Subtitle4 = styled.div`
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 10px;
+line-height: 160%;
+`;
+
+export const Subtitle5 = styled.div`
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 100%;
+`;
+
+/*Button*/
+
+export const Button = styled.button`
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 14px;
+line-height: 100%;
+`;
+
+/*캘린더 날짜*/
+
+export const CalDay = styled.div`
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 500;
+font-size: 12px;
+line-height: 100%;
+`;
+
+>>>>>>> Stashed changes
