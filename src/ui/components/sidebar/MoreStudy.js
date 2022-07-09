403a9abf891459aa7ea73e72204e472a4d0e4d42
haein.kwon <hae0_02ni@ewhain.net>
2022-07-09 22:00:24 +0900

    💄 SideBar.js 요소 추가

---
+++ src/ui/components/sidebar/MoreStudy.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import { BLACK100 } from "../../../core/colors";
+import { Button } from "../../../core/texts";
+
+export default function MoreStudy() {
+  return (
+    <div style={{marginTop:"58.58px"}}>
+    <MoreBtn>
+      <MoreStudyBtn>스터디 개설하기</MoreStudyBtn>
+    </MoreBtn>
+    </div>
+  );
+}
+
+const MoreBtn = styled.div`
+  width: 152px;
+  height: 48px;
+  background-color: ${BLACK100};
+  border-radius: 5px;
+`;
+
+const MoreStudyBtn = styled.div`
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 100%;
+  color: white;
+  text-align: center;
+  justify-content: baseline;
+  align-items: center;
+`;
--- src/ui/components/sidebar/SideBar.js
+++ src/ui/components/sidebar/SideBar.js
@@ -1,22 +1,47 @@
 import styled from "styled-components";
-import { gray100 } from "../../../core/colors";
-import { Subtitle2 } from "../../../core/texts";
-import logo from './SideBarLogo.png';
+import { BLACK100, gray100 } from "../../../core/colors";
+import { Body2, Subtitle2 } from "../../../core/texts";
+import logo from "./SideBarLogo.png";
+import coin from "./icon_coin.png";
+import MoreStudy from "./MoreStudy";
 
 export default function SideBar() {
-
-  
   return (
-  <SideBarBox>
-    <Logo src={logo}/>
-    <div style={{marginTop:'40px'}}>
-    <Subtitle2>MY STUDY</Subtitle2>
-    </div>
-    <
-  </SideBarBox>
+    <SideBarBox>
+      <Logo src={logo} />
+      <div style={{ marginTop: "40px" }}>
+        <Subtitle2>MY STUDY</Subtitle2>
+      </div>
+      <Bar />
+      <div
+        style={{
+          marginTop: "23px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Subtitle2>
+          이번주 벌금 정산 <Coin src={coin} />
+        </Subtitle2>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "nowrap",
+          marginTop: "23px",
+          alignItems: "center",
+        }}
+      >
+        <Body2>000000 </Body2>
+        <span style={{ marginLeft: "8px" }}>
+          <Subtitle2>원</Subtitle2>
+        </span>
+      </div>
+      <MoreStudy/>
+    </SideBarBox>
   );
 }
-
 
 const SideBarBox = styled.div`
   background-color: ${gray100};
@@ -26,11 +51,24 @@ const SideBarBox = styled.div`
   /* justify-content: center; */
   flex-direction: column;
   align-items: center;
-  
 `;
 
-const Logo= styled.img`
-width: 104px;
-height: 167px;
-margin-top: 40px;
+const Logo = styled.img`
+  width: 104px;
+  height: 167px;
+  margin-top: 40px;
 `;
+
+const Bar = styled.div`
+  width: 168px;
+  height: 3px;
+  border-radius: 5px;
+  margin-top: 23px;
+  background-color: ${BLACK100};
+`;
+
+const Coin = styled.img`
+  width: 15px;
+  height: 18px;
+`;
+
