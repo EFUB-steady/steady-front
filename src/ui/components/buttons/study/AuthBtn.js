2296bc4aa5bda9c493c056b589647e7e67009dba
doh-ha <hayeonlr@gmail.com>
2022-07-12 21:26:47 +0900

    💄인증하기 버튼 색깔 수정

--- src/ui/components/buttons/study/AuthBtn.js
+++ src/ui/components/buttons/study/AuthBtn.js
@@ -9,20 +9,21 @@ const Button = styled.button`
   height: 40px;
   border-radius: 5px;
   border: 3px solid black;
-  background: #f5f5f5;
+  background: black;
   text-align: center;
   font-family: "Pretendard";
   font-style: normal;
   font-weight: 700;
   font-size: 14px;
+  color: white;
 
   //margin-right: 216px;
   &:hover {
-    background: black;
-    color: white;
+    background: white;
+    color: black;
   }
   &:active {
-    background: black;
-    color: white;
+    background: white;
+    color: black;
   }
 `;
