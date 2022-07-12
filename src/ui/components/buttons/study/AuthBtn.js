33df3131ef69ee3d320d065043b31f7173b316da
doh-ha <hayeonlr@gmail.com>
2022-07-12 20:10:47 +0900

    💄인증하기 버튼 사용 및 css

--- src/ui/components/buttons/study/AuthBtn.js
+++ src/ui/components/buttons/study/AuthBtn.js
@@ -4,4 +4,25 @@ export default function AuthBtn() {
   return <Button>인증하기</Button>;
 }
 
-const Button = styled.button``;
+const Button = styled.button`
+  width: 108px;
+  height: 40px;
+  border-radius: 5px;
+  border: 3px solid black;
+  background: #f5f5f5;
+  text-align: center;
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+
+  //margin-right: 216px;
+  &:hover {
+    background: black;
+    color: white;
+  }
+  &:active {
+    background: black;
+    color: white;
+  }
+`;
--- src/ui/components/main/study-info/StudyInfo.js
+++ src/ui/components/main/study-info/StudyInfo.js
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 import { Title1 } from "../../../../core/texts";
+import AuthBtn from "./../../buttons/study/AuthBtn";
 
 export default function StudyInfo() {
   return (
     <InfoBox>
       <div style={{ display: "flex" }}>
         <Title1>Folio</Title1>
-        <div style={{ backgroundColor: "black", color: "white" }}>인증하기</div>
+        <AuthBtn />
       </div>
       <Content>
         Folio은 이화여자대학교 기반의 디자인 포트폴리오 작업 스터디입니다.
