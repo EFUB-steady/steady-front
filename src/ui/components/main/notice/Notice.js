ea04cd0bca10f446b3f7cb3cf2d2b96e182a2f57
haein.kwon <hae0_02ni@ewhain.net>
2022-07-14 16:39:10 +0900

    💄 notice component 설정

--- src/ui/components/main/notice/Notice.js
+++ src/ui/components/main/notice/Notice.js
@@ -1,10 +1,33 @@
 import NoticeItem from "./NoticeItem";
+import styled from "styled-components";
+import { Subtitle1 } from "../../../../core/texts";
+import noticeicon from "../../../../assets/icon_megaphone.png";
+
 
 export default function Notice() {
   return (
-    <div style={{ margin: 30 }}>
-      <div>공지사항</div>
+    <>
+    <Wrapper>
+      <NoticeIcon src={noticeicon} />
+      <Subtitle1>공지사항</Subtitle1>
+      </Wrapper>
+      <NoticeBox>
       <NoticeItem />
-    </div>
+      </NoticeBox>
+      </>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const NoticeIcon = styled.img`
+  height: 20px;
+  width: 20px;
+  margin-right: 10px;
+`;
+
+const NoticeBox = styled.div`
+margin-top: 16px;
+`
--- src/ui/components/main/notice/NoticeItem.js
+++ src/ui/components/main/notice/NoticeItem.js
@@ -1,3 +1,38 @@
+import styled from "styled-components";
+import { gray100 } from "../../../../core/colors";
+import { Body2 } from "../../../../core/texts";
+
 export default function NoticeItem() {
-  return <div>공지 내용들</div>;
+  return (
+    <Wrapper>
+      <Box>
+        <NoticeGroup>
+          <Body2>· 스터디는 3개월 간 진행합니다. (2022년 5월 - 7월)</Body2>
+          <Body2>· 모인 벌금은 그 달 스터디 인증을 가장 많이 한 1등이 독식합니다.</Body2>
+          <Body2>· 무단 결석은 3번까지 가능합니다. 3번 초과하면 이 스터디에서 탈퇴됩니다.</Body2>
+        </NoticeGroup>
+      </Box>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Box = styled.div`
+  width: 987px;
+  height: 170px;
+  border: 3px solid black;
+  background-color: ${gray100};
+  border-radius: 5px;
+`;
+
+const NoticeGroup = styled.div`
+  margin-left: 24px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  height: 100%;
+`;
