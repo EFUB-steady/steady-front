6c76dfec0f73447fec225ae5b1c0d268f2657578
Sojin Lee <isjift55@ewhain.net>
2022-07-15 01:42:07 +0900

    💄 벌금 규칙

--- src/ui/components/main/rule/Rule.js
+++ src/ui/components/main/rule/Rule.js
@@ -1,5 +1,4 @@
 import MainSubTitle from "../MainSubTitle";
-import RankingList from "../ranking/RankingList";
 import RuleItem from "./RuleItem";
 import noticeIcon from "../../../../assets/icon_coin.png";
 import styled from "styled-components";
@@ -8,7 +7,7 @@ export default function Rule() {
   return (
     <Wrapper>
       <MainSubTitle iconSrc={noticeIcon} title={"벌금 규칙"} />
-      <RankingList />
+      <RuleItem />
     </Wrapper>
   );
 }
--- src/ui/components/main/rule/RuleItem.js
+++ src/ui/components/main/rule/RuleItem.js
@@ -1,3 +1,48 @@
+import styled from "styled-components";
+import { gray100 } from "../../../../core/colors";
+import { Subtitle2 } from "../../../../core/texts";
+import RuleAuthDay from "./RuleAuthDay";
+import RuleAuthTime from "./RuleAuthTime";
+import RuleMoney from "./RuleMoney";
+
 export default function RuleItem() {
-  return <div>벌금 규칙들</div>;
+  return (
+    <Wrapper>
+      <RuleAuthBox>
+        <Subtitle2>지정 인증일</Subtitle2>
+        <RuleAuthDay />
+        <RuleAuthTime />
+      </RuleAuthBox>
+      <RuleMoneyBox>
+        <Subtitle2>결석 시</Subtitle2>
+        <RuleMoney />
+      </RuleMoneyBox>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: auto;
+  background-color: ${gray100};
+  border: 3px solid black;
+  border-radius: 5px;
+`;
+
+const RuleAuthBox = styled.div`
+  display: flex;
+  padding: 40px;
+  width: 74%;
+  border-right: 3px solid black;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const RuleMoneyBox = styled.div`
+  display: flex;
+  padding: 40px;
+  width: 26%;
+  justify-content: space-around;
+  align-items: center;
+`;
