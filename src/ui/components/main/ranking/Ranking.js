61d6f4759c14e3753e2b3e8942e5a7bb1ca56ddc
Sojin Lee <isjift55@ewhain.net>
2022-07-14 23:52:58 +0900

    💄 마진 간격 조절

--- src/ui/components/main/ranking/Ranking.js
+++ src/ui/components/main/ranking/Ranking.js
@@ -1,12 +1,17 @@
 import RankingList from "./RankingList";
 import rankicon from "../../../../assets/icon_rank.png";
 import MainSubTitle from "../MainSubTitle";
+import styled from "styled-components";
 
 export default function Ranking() {
   return (
-    <>
+    <Wrapper>
       <MainSubTitle iconSrc={rankicon} title={"0월 0주차 랭킹"} />
       <RankingList />
-    </>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  margin-bottom: 40px;
+`;
--- src/ui/components/main/rule/Rule.js
+++ src/ui/components/main/rule/Rule.js
@@ -1,10 +1,18 @@
+import MainSubTitle from "../MainSubTitle";
+import RankingList from "../ranking/RankingList";
 import RuleItem from "./RuleItem";
+import noticeIcon from "../../../../assets/icon_coin.png";
+import styled from "styled-components";
 
 export default function Rule() {
   return (
-    <div style={{ margin: 30 }}>
-      <div>벌금규칙</div>
-      <RuleItem />
-    </div>
+    <Wrapper>
+      <MainSubTitle iconSrc={noticeIcon} title={"벌금 규칙"} />
+      <RankingList />
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  margin-bottom: 40px;
+`;
