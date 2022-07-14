34f2423952303c78440ab646c2adf4473e5e7b70
doh-ha <hayeonlr@gmail.com>
2022-07-15 04:00:00 +0900

    💄로그인 버튼 컴포넌트 css

--- src/ui/components/buttons/user/LoginBtn.js
+++ src/ui/components/buttons/user/LoginBtn.js
@@ -4,4 +4,10 @@ export default function LoginBtn() {
   return <Button>로그인</Button>;
 }
 
-const Button = styled.button``;
+const Button = styled.button`
+  width: 77px;
+  height: 94px;
+  background: black;
+  border-radius: 5px;
+  color: white;
+`;
