1140f9acc4ec4de21adbdd7654583462bb60b9ba
doh-ha <hayeonlr@gmail.com>
2022-07-15 22:05:27 +0900

    💄버튼 컴포넌트 추가 및 css

--- src/ui/components/buttons/user/BackToLogin.js
+++ src/ui/components/buttons/user/BackToLogin.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
-export default function UserCheckBtn() {
-  return <Button>확인</Button>;
+export default function BackToLogin() {
+  return <Button>로그인 페이지로 돌아가기</Button>;
 }
 
 const Button = styled.button`
