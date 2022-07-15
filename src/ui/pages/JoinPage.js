25c975fe36fa9f8bf3b067c83b1d82c6dd77b20e
doh-ha <hayeonlr@gmail.com>
2022-07-16 00:18:43 +0900

    💄레이아웃 조정

--- src/ui/pages/JoinPage.js
+++ src/ui/pages/JoinPage.js
@@ -6,10 +6,28 @@ import SignInfo from "./../components/login/SignInfo";
 export default function JoinPage() {
   return (
     <div>
-      <LogoImg src={logo} />
-      <SignInfo />
-      <ExplanationPic />
+      <Wrapper>
+        <LeftContent>
+          <LogoImg src={logo} />
+          <SignInfo />
+        </LeftContent>
+        <RightContent>
+          <ExplanationPic />
+        </RightContent>
+      </Wrapper>
     </div>
   );
 }
 const LogoImg = styled.img``;
+const RightContent = styled.div``;
+const LeftContent = styled.div`
+  margin: 300px;
+  padding-left: 0px;
+`;
+const Wrapper = styled.div`
+  display: flex;
+  width: 90vw;
+  height: 100vh;
+  margin: 40px 90px;
+  padding-left: 0px;
+`;
