c0548f86a4053654b73eab86bd858169ef43ac87
doh-ha <hayeonlr@gmail.com>
2022-07-16 00:18:14 +0900

    💄material ui 사용

--- src/ui/components/login/InputArea.js
+++ src/ui/components/login/InputArea.js
@@ -1,30 +1,26 @@
-import styled from "styled-components";
+//import styled from "styled-components";
 
-import { hintColor } from "./../../../core/colors";
+//import { hintColor } from "./../../../core/colors";
 
 export default function InputArea() {
-  return (
-    <Input>
-      <input></input>;
-    </Input>
-  );
+  return <div></div>;
 }
 
-const Input = styled.div`
-  /*박스*/
-  width: 300px;
-  height: 42px;
-  background: #eeeeee;
-  border-radius: 5px;
-  margin-bottom: 8px;
-  /*글씨*/
-  color: ${hintColor};
-  font-family: "Pretendard";
-  font-style: normal;
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 100%;
-  display: flex;
-  align-items: center;
-  padding-left: 17.15px;
-`;
+// const Input = styled.div`
+//   /*박스*/
+//   width: 300px;
+//   height: 42px;
+//   background: #eeeeee;
+//   border-radius: 5px;
+//   margin-bottom: 8px;
+//   /*글씨*/
+//   color: ${hintColor};
+//   font-family: "Pretendard";
+//   font-style: normal;
+//   font-weight: 400;
+//   font-size: 14px;
+//   line-height: 100%;
+//   display: flex;
+//   align-items: center;
+//   padding-left: 17.15px;
+// `;
