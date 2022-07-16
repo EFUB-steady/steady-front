66842180dbaf3e90c49a73de86c8c6b692c4987f
doh-ha <hayeonlr@gmail.com>
2022-07-16 17:11:37 +0900

    💄FindIdPage.js
    인덱스 버튼 컴포넌트 디자인도 함

--- src/ui/pages/FindIdPage.js
+++ src/ui/pages/FindIdPage.js
@@ -1,3 +1,13 @@
+import styled from "styled-components";
+import logo from "../../assets/logo_horizontal.png";
+import FindBox from "../components/login/FindBox";
+
 export default function FindIdPage() {
-  return <div>FindId Page</div>;
+  return (
+    <div>
+      <LogoImg src={logo} />
+      <FindBox />
+    </div>
+  );
 }
+const LogoImg = styled.img``;
