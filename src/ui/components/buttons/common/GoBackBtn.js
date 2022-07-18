bb3f03d16e55741870f37063e32b45177e59b8d2
sj-lee33 <isjift55@ewhain.net>
2022-07-18 22:00:23 +0900

    ✍️ 스터디 개설하기 페이지>뒤로가기 구현

--- src/ui/components/buttons/common/GoBackBtn.js
+++ src/ui/components/buttons/common/GoBackBtn.js
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 import goBackIcon from "../../../../assets/icon_undo.png";
+import { useNavigate } from "react-router-dom"; // Import react-route-dom
+
 export default function GoBackBtn() {
-  return <BackImg src={goBackIcon} />;
+  const navigate = useNavigate();
+  return <BackImg src={goBackIcon} onClick={() => navigate(-1)} />;
 }
 
 const BackImg = styled.img`
   height: 45px;
   width: 45px;
   margin-bottom: 28px;
+  &:hover {
+    cursor: pointer;
+  }
 `;
