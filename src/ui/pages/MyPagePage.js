c18c8f6f38fd931dda9c7494a1f1cfd7e93e421f
journey_park <80277274+jihyeun-park@users.noreply.github.com>
2022-07-19 03:44:53 +0900

    💄 MyPagePage 배치

--- src/ui/pages/MyPagePage.js
+++ src/ui/pages/MyPagePage.js
@@ -1,3 +1,28 @@
+import styled from "styled-components";
+import Header from "../components/header/Header";
+import SideBar from "../components/sidebar/SideBar";
+
 export default function MyPagePage() {
-  return <div>MyPage Page</div>;
+  return (
+    <Wrapper>
+      <SideBar />
+      <MainContext>
+        <Header />
+        <div>My Page</div>
+      </MainContext>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const MainContext = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  margin: 58px 90px 58px 288px;
+`;
