3db5654514fc607bab7e6fb00005c4efd903504b
doh-ha <hayeonlr@gmail.com>
2022-07-25 14:43:12 +0900

    💄나의 인증 목록 캘린더 충돌 해결

--- src/ui/components/mypage/MyAuthList.js
+++ src/ui/components/mypage/MyAuthList.js
@@ -19,9 +19,11 @@ const MyAuthList = () => {
 export default MyAuthList;
 
 const Text = styled.div`
-  width: 150px;
-  height: 50px;
+  width: 200px;
+  height: 21px;
   margin-top: 40px;
+  margin-bottom: 23px;
+
   font-family: "Pretendard";
   font-style: normal;
   font-weight: 700;
