8e54eb82315ed7c3e889aef8c18e701e17116aea
journey_park <80277274+jihyeun-park@users.noreply.github.com>
2022-07-26 07:05:13 +0900

    💄 DateAndBtn 컴포넌트

--- src/ui/components/mypage/DateAndBtn.js
+++ src/ui/components/mypage/DateAndBtn.js
@@ -1,9 +1,46 @@
 import styled from "styled-components";
+import { format } from "date-fns";
 
 export default function DateAndBtn() {
+  const today = new Date();
+  const formattedToday = format(today, "MM월 dd일");
+
   return (
     <>
-      <p>날짜와 버튼</p>
+      <TitleBox>
+        <Title>{formattedToday}</Title>
+        <Button>저장하기 </Button>
+      </TitleBox>
     </>
   );
 }
+
+const TitleBox = styled.div`
+  display: flex;
+  height: auto;
+  justify-content: space-between;
+`;
+
+const Title = styled.div`
+  font-size: 40px;
+  font-weight: 500;
+  color: black;
+  line-height: 100%;
+  margin-top: 3rem;
+`;
+
+const Button = styled.button`
+  width: 108px;
+  height: 40px;
+  background: black;
+  border-radius: 5px;
+  color: white;
+  margin: 4px;
+  text-align: center;
+
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 100%;
+  margin-top: 3rem;
+`;
