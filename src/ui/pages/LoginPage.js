472c8b6ffc189481b8c4fbbb45174e487f2ac612
doh-ha <hayeonlr@gmail.com>
2022-07-25 13:56:14 +0900

    💄LoginPage.js vw로 수정 및 absolute 삭제

--- src/ui/pages/LoginPage.js
+++ src/ui/pages/LoginPage.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import Login from "../components/login/Login";
 import pic from "../../assets/web_explanation_pic.png";
-import { gray300 } from "../../core/colors";
 
 export default function LoginPage() {
   return (
@@ -17,7 +16,7 @@ export default function LoginPage() {
 }
 
 const Wrapper = styled.div`
-  width: 100%;
+  width: 100vw;
   height: 100%;
   display: flex;
   flex-direction: row;
@@ -25,25 +24,13 @@ const Wrapper = styled.div`
 `;
 const LeftContent = styled.div`
   width: 400rem;
-  float: left;
   box-sizing: border-box;
-  margin-top: 200px;
-  margin-left: 350px;
-
-  padding-right: 215px;
-`;
-
-const RightContent = styled.div`
-  position: absolute;
-  width: 640px;
-  height: 1024px;
-  left: 1250px;
-  top: 0px;
-
-  background: ${gray300};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 10rem;
 `;
 
 const Pic = styled.img`
-  width: 500rem;
   height: 750px;
 `;
