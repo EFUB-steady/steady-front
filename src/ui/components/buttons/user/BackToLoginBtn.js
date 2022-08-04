2d549a6b16f057a3d01c882640367ae74853a09c
doh-ha <hayeonlr@gmail.com>
2022-08-04 19:33:37 +0900

    🚚로그인페이지로 돌아가기 구현

--- src/ui/components/buttons/user/BackToLoginBtn.js
+++ src/ui/components/buttons/user/BackToLoginBtn.js
@@ -1,7 +1,11 @@
 import styled from "styled-components";
-
+import CustomLink from "../../commons/CustomLink";
 export default function BackToLoginBtn() {
-  return <Button>로그인 페이지로 돌아가기</Button>;
+  return (
+    <CustomLink to="/">
+      <Button>로그인 페이지로 돌아가기</Button>;
+    </CustomLink>
+  );
 }
 
 const Button = styled.button`
