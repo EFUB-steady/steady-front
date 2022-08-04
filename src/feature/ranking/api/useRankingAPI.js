94ac48359c30d9149906784ed2b6c9810abf04ed
SJ-Lee33 <77518932+SJ-Lee33@users.noreply.github.com>
2022-08-04 21:27:30 +0900

    🧹 Lint

--- src/feature/ranking/api/useRankingAPI.js
+++ src/feature/ranking/api/useRankingAPI.js
@@ -7,14 +7,12 @@ export const useRankingAPI = () => {
   const { selectedStudyId } = useSelectedStudyId();
 
   const rankingAPI = async () => {
-    console.log("here!");
     try {
       const { data } = await axiosInstanceHeader.get(
         `studies/ranking/${selectedStudyId}`,
       );
       if (data) {
         setRanking(data);
-        console.log("useRankingAPI: ", data);
       }
     } catch (error) {
       console.log("rankingAPI fail..");
