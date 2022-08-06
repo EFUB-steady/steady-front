ee86f94e56228055ffc8251b1dbf65fd76ae54f8
haein.kwon <hae0_02ni@ewhain.net>
2022-08-06 19:50:59 +0900

    ✍️  스터디 개설 저장하기 버튼 기능 추가

--- src/ui/components/buttons/study/SaveBtn.js
+++ src/ui/components/buttons/study/SaveBtn.js
@@ -1,14 +1,72 @@
 import styled from "styled-components";
+import { useMakeStudy } from "../../../../feature/MakeStudy/recoil/useMakeStudy";
+import { useMakeStudyAPI } from "../../../../feature/MakeStudy/api/useMakeStudyAPI";
 
 export default function SaveBtn() {
-  return <Button>저장하기</Button>;
+  const { MakeStudyReset } = useMakeStudy();
+  const { makeStudyAPI } = useMakeStudyAPI();
+
+  const {
+    name,
+    summary,
+    description,
+    mon,
+    thu,
+    wed,
+    tue,
+    fri,
+    sat,
+    sun,
+    minute,
+    hour,
+    account,
+    accountBank,
+    accountName,
+    money,
+    lateMoney,
+  } = useMakeStudy();
+
+  const makeStudyHandler = () => {
+    if (
+      name !== "" &&
+      summary !== "" &&
+      description !== "" &&
+      mon == false &&
+      thu == false &&
+      wed == false &&
+      tue == false &&
+      fri == false &&
+      sat == false &&
+      sun == false &&
+      minute !== 0 &&
+      hour !== 0 &&
+      accountBank !== "" &&
+      accountName !== "" &&
+      account !== "" &&
+      money !== 0 &&
+      lateMoney !== 0
+    ) {
+      makeStudyAPI({
+        onSuccess: () => {
+          MakeStudyReset();
+          console.log("추가했음");
+        },
+        onFail: () => {
+          console.log("스터디 개설 실패");
+        },
+      });
+    } else {
+      console.log("아무것도 아님");
+    }
+  };
+  return <Button onClick={() => makeStudyHandler()}>저장하기</Button>;
 }
 
 const Button = styled.button`
   background-color: black;
   color: white;
-  width: 81px;
-  height: 36px;
+  width: 90px;
+  height: 50px;
   border-radius: 5px;
   font-size: 14px;
   font-weight: 700;
