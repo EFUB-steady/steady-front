ff3acd408acb7f2ddf40e8b5531d6b9f3783c5d0
haein.kwon <hae0_02ni@ewhain.net>
2022-08-06 12:01:42 +0900

    🧹 주석삭제

--- src/ui/modal/modals/ReportMoreModal.js
+++ src/ui/modal/modals/ReportMoreModal.js
@@ -6,13 +6,6 @@ import ReportMoreModalTitle from "../recoil/components/ReportMoreModalTitle";
 import { ReportStudyModalPic } from "../recoil/components/ReportStudyModalPic";
 import { StudyModalUrl } from "../recoil/components/StudyModalUrl";
 
-{
-  /* 피그마의 최종 신고 자세히 보기 부분 Modal창입니다. 현재는
-Modal이 띄워지지 않으며, 모달을 띄우기 위해 notion에 적혀 있는 대로
-완료했으며, 자세히보기 버튼을 눌러야 띄워져야 합니다.
-자세히 보기 버튼은 ReportMoreBtn.js 이며, ReportItem.js 부분에서 호출되어 있습니다.*/
-}
-
 export default function ReportMoreModal() {
   const { isOpen, closeModal } = useReportMoreModal();
 
