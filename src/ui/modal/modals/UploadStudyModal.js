44320bc1cc0461f9a4edb1c0ccaeaa1ae7955a99
SJ-Lee33 <77518932+SJ-Lee33@users.noreply.github.com>
2022-08-06 10:55:49 +0900

    ♻️ UploadStudyModal에서 recoil 빼기

--- src/ui/modal/modals/UploadStudyModal.js
+++ src/ui/modal/modals/UploadStudyModal.js
@@ -2,17 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import { Dialog, DialogContent } from "@mui/material";
 import StudyModalTitle from "../recoil/components/StudyModalTitle";
-import { useUploadStudyModal } from "../recoil/hooks/useModals";
 import { StudyModalPic } from "../recoil/components/StudyModalPic";
 import { StudyModalUrl } from "../recoil/components/StudyModalUrl";
 
-export default function UploadStudyModal() {
-  const { isOpen, closeModal } = useUploadStudyModal();
-
+export default function UploadStudyModal({ isOpen, setIsOpen }) {
   return (
     <Dialog
       open={isOpen}
-      onClose={closeModal}
+      onClose={() => setIsOpen(false)}
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
       maxWidth="false"
