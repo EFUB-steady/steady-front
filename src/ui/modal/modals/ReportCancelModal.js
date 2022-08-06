10551fcab95bb60d37a64455d15ae88ccbc939c9
haein.kwon <hae0_02ni@ewhain.net>
2022-08-06 12:41:04 +0900

    🔥ReportCancel- 삭제, modal자체에 합쳤습니다

--- src/ui/modal/modals/ReportCancelModal.js
+++ src/ui/modal/modals/ReportCancelModal.js
@@ -2,7 +2,10 @@ import React from "react";
 import styled from "styled-components";
 import { Dialog, DialogContent } from "@mui/material";
 import { useReportCancelModal } from "../recoil/hooks/useModals";
-import ReportCancel from "../recoil/components/ReportCancel";
+
+import { Body2, Subtitle2 } from "../../../core/texts";
+import AuthCancelYesBtn from "../../../ui/components/buttons/study/AuthCancelYesBtn";
+import AuthCancelNoBtn from "../../../ui/components/buttons/study/AuthCancelNoBtn";
 
 export default function ReportCancelModal() {
   const { isOpen, closeModal } = useReportCancelModal();
@@ -18,7 +21,21 @@ export default function ReportCancelModal() {
     >
       <ContentWrapper>
         <DialogContent>
-          <ReportCancel />
+          <Wrapper>
+            <Subtitle2>인증 취소</Subtitle2>
+            <SecondWrapper>
+              <Body2>00000님 05월 00일자</Body2>
+              <Body2>인증을 취소하시겠습니까?</Body2>
+            </SecondWrapper>
+            <ButtonWrapper>
+              <div style={{ marginRight: "12px" }}>
+                <AuthCancelYesBtn />
+              </div>
+              <div>
+                <AuthCancelNoBtn />
+              </div>
+            </ButtonWrapper>
+          </Wrapper>
         </DialogContent>
       </ContentWrapper>
     </Dialog>
@@ -26,3 +43,25 @@ export default function ReportCancelModal() {
 }
 
 const ContentWrapper = styled.div``;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SecondWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 16px;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  margin-top: 16px;
+  justify-content: center;
+  align-items: center;
+`;
