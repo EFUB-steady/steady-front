6d4361f2b15d0cd0985886bb3df29a37e7309cd4
haein.kwon <hae0_02ni@ewhain.net>
2022-08-07 00:33:30 +0900

    💄 마이페이지 ui 개선중

--- src/ui/components/mypage/MyAuthCal.js
+++ src/ui/components/mypage/MyAuthCal.js
@@ -1,17 +1,67 @@
 import styled from "styled-components";
 import React, { useState } from "react";
-import DatePicker from "react-datepicker";
+import DatePicker, { registerLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import ko from "date-fns/locale/ko";
+import { getYear } from "date-fns";
+registerLocale("ko", ko);
+const _ = require("lodash");
 
 const MyAuthList = () => {
   const [startDate, setStartDate] = useState(new Date());
+
+  const years = _.range(1990, getYear(new Date()) + 1, 1);
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
   return (
     <>
       <Text>나의 인증 목록</Text>
       <DatePicker
+        showPopperArrow={false}
+        fixedHeight
+        style={{
+          content: "e99c",
+          position: "absolute",
+          fontSize: "1rem",
+        }}
         dateFormat="yyyy/MM/dd"
         selected={startDate}
         onChange={(date) => setStartDate(date)}
+        renderCustomHeader={({
+          date,
+          decreaseMonth,
+          increaseMonth,
+          preMonthbuttonDisabled,
+          nextMonthButtonDisabled,
+        }) => (
+          <div>
+            {" "}
+            <button
+              onClick={decreaseMonth}
+              disabled={preMonthbuttonDisabled}
+            ></button>
+            <div>
+              {date.getFullYear()}년 {months[date.getMonth()]}
+              <button
+                onClick={increaseMonth}
+                disabled={nextMonthButtonDisabled}
+              ></button>
+            </div>
+          </div>
+        )}
       />
     </>
   );
