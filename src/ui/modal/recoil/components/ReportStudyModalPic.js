dfd0dc0c475aaba92a3a7023954ecbc8321ae6ba
haein.kwon <hae0_02ni@ewhain.net>
2022-08-06 12:15:54 +0900

    💄 스터디 신고 모달 사진 업로드

--- src/ui/modal/recoil/components/ReportStudyModalPic.js
+++ src/ui/modal/recoil/components/ReportStudyModalPic.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { gray100, hintColor } from "../../../../core/colors";
 import icon from "../../../../assets/icon_picture_card_mypage.png";
 import { Subtitle2 } from "../../../../core/texts";
+import studyPic from "../../../../assets/studypic.jpg";
 
 //신고_자세히보기 부분의 사진 인증 컴포넌트 유아이
 export function ReportStudyModalPic() {
@@ -13,6 +14,9 @@ export function ReportStudyModalPic() {
           <div style={{ marginTop: "11px" }}>사진 인증하기</div>
         </Subtitle2>
       </TitleWrapper>
+      <PicWrapper>
+        <StudyPic src={studyPic} />
+      </PicWrapper>
     </Wrapper>
   );
 }
@@ -28,7 +32,7 @@ const Wrapper = styled.div`
 `;
 const TitleWrapper = styled.div`
   width: 914px;
-  height: 40px;
+  height: 8%;
   background-color: #ffffff;
   border-bottom: 3px solid #000000;
   display: flex;
@@ -40,4 +44,17 @@ const TitleIcon = styled.img`
   margin-left: 17px;
   margin-right: 9px;
   margin-top: 9px;
+`;
+
+const StudyPic = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+const PicWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 92%;
+  flex-direction: column;
 `;
