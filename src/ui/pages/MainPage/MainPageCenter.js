1e4b2cad54adf3bbb61d4d7d0036d962d7fc6e09
SJ-Lee33 <77518932+SJ-Lee33@users.noreply.github.com>
2022-08-07 13:39:24 +0900

    ♻ api 연결 로직 재검토

--- src/ui/pages/MainPage/MainPageCenter.js
+++ src/ui/pages/MainPage/MainPageCenter.js
@@ -1,10 +1,13 @@
 import { useEffect } from "react";
 import styled from "styled-components";
+import { useNoticeOneAPI } from "../../../feature/notices/noticeOne/api/useNoticeOneAPI";
+import { useRankingAPI } from "../../../feature/ranking/api/useRankingAPI";
 import { useSelectedStudyAPI } from "../../../feature/studies/studySelect/api/useSelectedStudyAPI";
 import {
   useSelectedStudyId,
   useSelectedStudyIsLeader,
 } from "../../../feature/studies/studySelect/recoil/useSelectedStudy";
+import { useUserAPI } from "../../../feature/user/api/useUserAPI";
 import SideBar from "../../components/sidebar/SideBar";
 import HostMainPage from "./HostMainPage";
 import MemberMainPage from "./MemberMainPage";
@@ -13,9 +16,15 @@ export default function MainPageCenter() {
   const { selectedStudyId } = useSelectedStudyId();
   const { selectedStudyAPI } = useSelectedStudyAPI();
   const { isLeader } = useSelectedStudyIsLeader();
+  const { noticeOneAPI, isLoading: isNoticeLoading } = useNoticeOneAPI();
+  const { rankingAPI, isLoading: isRankingLoading } = useRankingAPI();
+  const { userAPI } = useUserAPI();
 
   useEffect(() => {
     selectedStudyAPI();
+    rankingAPI();
+    noticeOneAPI();
+    userAPI();
   }, [selectedStudyId]);
 
   return (
